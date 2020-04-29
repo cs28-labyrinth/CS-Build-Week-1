df37e4bc82a0869f98e8d984955a3d530b932487
Jasmine <jasminecharles@hotmail.com>
2020-04-28 17:11:50 -0700

    testing

---
+++ util/item_generator.py
@@ -0,0 +1,13 @@
+
+
+
+class Item:
+    def __init__(self, item_name, item_description):
+        self.item_name = item_name
+        self.item_description = item_description
+
+        
+
+    def __str__(self):
+            return f"Item: {self.item_name}, Item Description: {self.item_description}"
+            
